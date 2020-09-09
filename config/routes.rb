d48afc20ddca2cd486c73536fb7283b8f498c2c1
debian <jhang0912407249@gmail.com>
2020-09-09 05:07:06 -0400

    facebook oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   resources :orders
-  get 'checkout',to: 'orders#checkout'
-  get 'carts/add'
+  get "checkout", to: "orders#checkout"
+  get "carts/add"
   resources :products
-  root to: 'pages#home'
-  devise_for :users
-  get '/search',to: 'search#search'
-  post '/search',to: 'search#result'
-  resource :cart, only:[:show, :destroy] do
-	      collection do
-	            post :add, path:'add/:id'
-	      end
+  root to: "pages#home"
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth" }
+  get "/search", to: "search#search"
+  post "/search", to: "search#result"
+  resource :cart, only: [:show, :destroy] do
+    collection do
+      post :add, path: "add/:id"
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
